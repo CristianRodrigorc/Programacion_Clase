d3d2e32c943f271b155c7379ccd75800be1e104f
CristianRodrigorc <cristian24121997@gmail.com>
2024-04-02 17:27:07 +0200

    nuevo archivo java

---
+++ Calculadora/src/cod/ClaseMain.java
@@ -0,0 +1,19 @@
+package cod;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class ClaseMain {
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		System.out.println("Indique la cantidad de objetos a calcular");
+		int cantidad = sc.nextInt();
+		
+		int [] miObjeto = new int [cantidad];
+		
+		for (int i = 0; i < miObjeto.length; i++) {
+			
+		}
+	}
+}
